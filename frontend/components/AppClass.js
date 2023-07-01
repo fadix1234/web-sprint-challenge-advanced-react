5895465a3aab5528a702dcb3bee58da3ed6ddcf6
fadix1234 <ahmadzaiyasmin@yahoo.com>
2023-07-01 00:54:33 -0700

    slow

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -21,38 +21,67 @@ export default class AppClass extends React.Component {
    Message:'',
    Email:'',
    Steps:0,
-   Index:4, // the index the "B" is at
-   X: [1,2,3],
-   Y: [1,2,3],
-   };
+   index:4, // the index the "B" is at
+   x: 2,
+   y: 2,
+   }
     
 
 
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
+    getXY = () => {
+    const { index } = this.state
+    const x = (index % 3) + 1
+    let y
+    if (index < 3) y = 1
+    else if (index < 6) y = 2
+    else if (index < 9) y = 3
+    this.state.x = x
+    this.state.y = y
+    return [x, y]
+    }
+  
+    
+    
+   
+    
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
+  
 
-  getXYMessage = () => {
+   getXYMessage = () => {
+    const Cor = `(${this.state.x},${this.state.y})`
+    console.log(Cor);
+    return (Cor)
+    
+         
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
-  }
+   }
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
   }
 
   getNextIndex = (direction) => {
+    switch(direction){case 'up':
+    return (index < 3) ? index : index - 3
+  case 'down':
+    return (index > 5) ? index : index + 3
+  case 'left':
+    return (index % 3 === 0) ? index : index - 1
+  case 'right':
+    return ((index - 2) % 3 === 0) ? index : index + 1}
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
   }
 
   move = (evt) => {
+    
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
@@ -72,7 +101,7 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
+          <h3 id="coordinates">Coordinates {this.getXYMessage()}</h3>
           <h3 id="steps">You moved {Steps} times</h3>
         </div>
         <div id="grid">
